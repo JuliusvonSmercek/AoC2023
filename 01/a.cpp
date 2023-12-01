9e63cc7502425104537b1edd26c2e27f55daab19
Julius <juliusvonsmercek@protonmail.com>
2023-12-01 04:09:00 +0100

    init AoC2023

---
+++ 01/a.cpp
@@ -0,0 +1,14 @@
+#include "util.h"
+
+long long solve(vector<string> &lines) {
+  // for (string &line : lines) {
+  //   stringstream ss(line);
+  // }
+  // vector<vector<char>> data = readMatrix(lines);
+  // vector<vector<int>> data = readInput<int>(lines);
+  long long result = 0;
+
+  
+
+  return result;
+}
